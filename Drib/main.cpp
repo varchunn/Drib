aecd0e7295d2b487d12a90b37732631b943f1235
varchunn <varvaram186@gmail.com>
2024-09-15 18:59:31 +0300

    Initial Commit

---
+++ Drib/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Drib
+//
+//  Created by Варя Мінєнкова on 15.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
